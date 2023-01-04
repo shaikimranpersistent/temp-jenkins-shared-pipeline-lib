a176c328167a18d985026717540715c25a1c2adf
shaikimranpersistent <121207524+shaikimranpersistent@users.noreply.github.com>
2023-01-04 11:40:19 +0000

    ok for now

--- vars/runPipeline.groovy
+++ vars/runPipeline.groovy
@@ -1,20 +1,27 @@
 def call() {
         p = new printingMethods()
+
         p.printStageStartMsg("Authenitcation");
         authenticateServiceAccount();
+        p.printStageSuccessMsg("Authenitcation")
 
         p.printStageStartMsg("Configuring Docker Package Loc");
         configureDockerPackageLoc();
+        p.printStageSuccessMsg("Configuring Docker Package Loc");
 
         p.printStageStartMsg("CSR repo cloning");
         csrCloneRepo();
+        p.printStageSuccessMsg("CSR repo cloning");
 
         p.printStageStartMsg("Building Docker Image");
         buildDockerImage();
+        p.printStageSuccessMsg("Building Docker Image");
 
         p.printStageStartMsg("Pushing Image to Artifact Registry");
         pushImageToArtifactRegistry();
+        p.printStageSuccessMsg("Pushing Image to Artifact Registry");
 
         p.printStageStartMsg("Attest Image");
         attestImage();
+        p.printStageSuccessMsg("Attest Image");
 }
