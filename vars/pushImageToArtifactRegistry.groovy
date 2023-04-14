47e3f305262436b8118942ae616305a13b2197b0
BeastImranPersistent <shaik_imran@persistent.com>
2023-04-14 19:46:47 +0000

    add CONTAINER_PATH

--- vars/pushImageToArtifactRegistry.groovy
+++ vars/pushImageToArtifactRegistry.groovy
@@ -1,6 +1,5 @@
 def call() {
     sh """
-        docker tag $IMAGE_NAME $REGISTRY_LOC/$PROJECT_ID/$ARTIFACT_REG/$IMAGE_NAME:$IMAGE_TAG
-        docker push $REGISTRY_LOC/$PROJECT_ID/$ARTIFACT_REG/$IMAGE_NAME:$IMAGE_TAG
+        docker push $CONTAINER_PATH
     """
 }
