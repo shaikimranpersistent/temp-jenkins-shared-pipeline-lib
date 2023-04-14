98e201b2b790c305f5d6d7df05a738e3d7f02571
BeastImranPersistent <shaik_imran@persistent.com>
2023-04-14 18:52:02 +0000

    fix image name use incosistency

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -2,6 +2,6 @@ def call() {
     sh """
         cd $REPO_NAME
         git checkout $BRANCH_NAME 
-        docker build -t $REGISTRY_LOC/$PROJECT_ID/$ARTIFACT_REG/$IMAGE_NAME:$IMAGE_TAG .
+        docker build -t $IMAGE_NAME:$IMAGE_TAG .
     """
 }
--- vars/pushImageToArtifactRegistry.groovy
+++ vars/pushImageToArtifactRegistry.groovy
@@ -1,5 +1,6 @@
 def call() {
     sh """
+        docker tag $IMAGE_NAME $REGISTRY_LOC/$PROJECT_ID/$ARTIFACT_REG/$IMAGE_NAME:$IMAGE_TAG
         docker push $REGISTRY_LOC/$PROJECT_ID/$ARTIFACT_REG/$IMAGE_NAME:$IMAGE_TAG
     """
 }
