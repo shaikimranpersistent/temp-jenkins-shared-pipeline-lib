f5fad3e8199e9c287b3f16981fc327e14cc910a3
BeastImranPersistent <shaik_imran@persistent.com>
2023-04-14 19:10:10 +0000

    user CONTAINER_PATH

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -2,6 +2,6 @@ def call() {
     sh """
         cd $REPO_NAME
         git checkout $BRANCH_NAME 
-        docker build -t $IMAGE_NAME:$IMAGE_TAG .
+        docker build -t $CONTAINER_PATH .
     """
 }
