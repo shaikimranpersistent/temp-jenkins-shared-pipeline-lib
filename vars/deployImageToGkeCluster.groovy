533fe5d2255ab3bb061fa367498dceaeef822756
BeastImranPersistent <shaik_imran@persistent.com>
2023-04-14 21:05:00 +0000

    add kubectl apply for Service.yaml

--- vars/deployImageToGkeCluster.groovy
+++ vars/deployImageToGkeCluster.groovy
@@ -2,6 +2,7 @@ def call() {
     sh """
         gcloud container clusters get-credentials $GKE_CLUSTER_NAME --zone $GKE_CLUSTER_ZONE --project $PROJECT_ID
         cd $REPO_NAME
+        kubectl apply -f Service.yaml
         kubectl apply -f Deployment.yaml
     """
 }
